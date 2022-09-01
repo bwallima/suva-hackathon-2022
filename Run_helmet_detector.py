35a92d9b8ccddfd26b5fad16b90cb6d7e5f68989
MarioBucher <mario.b@gmx.ch>
2022-09-01 18:38:58 +0200

    add initial trafficlight gui

--- Run_helmet_detector.py
+++ Run_helmet_detector.py
@@ -3,6 +3,7 @@ from main import cyclistCounter, helmet_detection
 from imutils import paths
 import cv2
 import os
+from gui import trafficlight
 
 ###Initializing or helmet detection and counter
 #Instantiating our helmet class
@@ -23,7 +24,7 @@ image_paths = list(paths.list_images("images"))
 #Looping through every individual path
 
 cam = cv2.VideoCapture(0)
-
+trafficLight = trafficlight.TrafficLight()
 while True:
     check, image = cam.read()
     image_path = '/home/bwallima/PycharmProjects/Cyclists-Helmet-Detection/temp_image.jpg'
@@ -36,11 +37,20 @@ while True:
     print('\n')
     #Feeding our individual image to our Helmet Detection Class
     detected_img = cyclistCounter.image_detect(image_path, cnt_cyclist)
-    frame, outs = helmet_detection.get_detection(frame=image,image_path=image_path, copy_frame=image, numb_cyclist = detected_img, cnt_helmets=cnt_helmets, cnt_no_helmets=cnt_no_helmets)
-    
-    """
-    Press Esc Key to go through the processed images one by one in the Window.
-    """
-#Print for Fun
-print("Total Cyclists wearing Helmets are {}".format(sum(cnt_helmets)))
-print("Total Cyclists without Helmets are {}".format(sum(cnt_no_helmets)))
+    frame, outs,  numb_cyclist, cnt_helmets, cnt_no_helmets = helmet_detection.get_detection(frame=image,image_path=image_path, copy_frame=image, numb_cyclist = detected_img , cnt_helmets=cnt_helmets, cnt_no_helmets=cnt_no_helmets)
+
+    if numb_cyclist > 0:
+        if (cnt_helmets[-1] > 0 and cnt_helmets[-1] > cnt_no_helmets[-1]):
+            #smile
+            trafficLight.update_image(1)
+        elif cnt_no_helmets[-1] > 0:
+            #cry
+            trafficLight.update_image(2)
+        else:
+            #neutral
+            trafficLight.update_image(0)
+    else:
+        #neutral
+        trafficLight.update_image(0)
+
+
