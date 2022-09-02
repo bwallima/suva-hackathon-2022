77e90c1b592083fe5e4121ebb2192afac2b5ca78
bwallima <bwallima@student-ethz.ch>
2022-09-02 07:38:09 +0200

    small updates

--- Run_helmet_detector.py
+++ Run_helmet_detector.py
@@ -1,36 +1,36 @@
-#Importing necessary Libraries and Packages.
 from main import cyclistCounter, helmet_detection
-from imutils import paths
 import cv2
-import os
+import time
 
-###Initializing or helmet detection and counter
-#Instantiating our helmet class
 helmet_detection = helmet_detection()
-
-#Empty lists for multiple counts.
 cnt_cyclist = []
 cnt_helmets = []
 cnt_no_helmets = []
 
-cam = cv2.VideoCapture(0)
+# load models
 model, classes, colors, output_layers = cyclistCounter.load_yolo()
+modelConfiguration = "yolo_helmet/yolov3-obj.cfg"
+modelWeights = "yolo_helmet/yolov3-obj_2400.weights"
+net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
+cam = cv2.VideoCapture(0)
 
 while True:
+    start = time.time()
     check, image = cam.read()
+
     image = cv2.resize(image, dsize=(640, 480), interpolation=cv2.INTER_AREA)
     height, width, channels = image.shape
 
-    print('\n')
-    #Feeding our individual image to our Helmet Detection Class
-    detected_img = cyclistCounter.image_detect(image, cnt_cyclist, height, width, model, classes, colors, output_layers)
-    frame, outs = helmet_detection.get_detection(frame=image, copy_frame=image, numb_cyclist = detected_img,
-                                                 cnt_helmets=cnt_helmets, cnt_no_helmets=cnt_no_helmets)
-    
-    """
-    Press Esc Key to go through the processed images one by one in the Window.
-    """
-#Print for Fun
-print("Total Cyclists wearing Helmets are {}".format(sum(cnt_helmets)))
-print("Total Cyclists without Helmets are {}".format(sum(cnt_no_helmets)))
+    # Feeding our individual image to our Helmet Detection Class
+    numb_cyclist = cyclistCounter.image_detect(image, cnt_cyclist, height, width, model, classes, colors,
+                                                        output_layers)
+    if numb_cyclist == 0:
+        print(time.time())
+        continue
+    helmet_detection.get_detection(frame=image, copy_frame=image, numb_cyclist=numb_cyclist,
+                                   cnt_helmets=cnt_helmets, cnt_no_helmets=cnt_no_helmets, net=net)
+
+    print(f"Loop time: {time.time() - start}")
